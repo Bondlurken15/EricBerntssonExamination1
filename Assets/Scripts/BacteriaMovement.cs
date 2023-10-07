b383eda6795988b8b8bc475f954f6cadaaacde5d
bonasieric <eric.berntsson@elev.ga.lbs.se>
2023-10-07 14:20:09 +0200

    Wobble and Move together

--- Assets/Scripts/BacteriaMovement.cs
+++ Assets/Scripts/BacteriaMovement.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wobble : MonoBehaviour
+public class BacteriaMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] float xMove = 0.02f;
+    [SerializeField] float yMove = 0.02f;
+    [Header("Wobble")]
     [SerializeField] float xWobble = 0.002f;
     [SerializeField] float yWobble = 0.002f;
     [SerializeField] float xScaleExcess = 0.1f;
@@ -24,8 +28,17 @@ public class Wobble : MonoBehaviour
 
     void Update()
     {
-        // should probably make it so the things can't look bad
-        
+        Move();
+        Wobble();
+    }
+
+    void Move()
+    {
+        transform.position = transform.position + new Vector3(Random.Range(-xMove, xMove), Random.Range(-yMove, yMove));
+    }
+
+    void Wobble()
+    {
         if (transform.localScale.x > maxLocalScale.x)
         {
             transform.localScale = transform.localScale - new Vector3(xScaleCorrection, 0, 0);
