117c26746cf09ca6b17d7fe667c334554c0a5834
bonasieric <eric.berntsson@elev.ga.lbs.se>
2023-10-07 13:58:40 +0200

    Made prefabs and changed wobble script

--- Assets/Scripts/Wobble.cs
+++ Assets/Scripts/Wobble.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Wobble : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float xWobble = 0.002f;
+    [SerializeField] float yWobble = 0.002f;
+    [SerializeField] float xScaleExcess = 0.1f;
+    [SerializeField] float yScaleExcess = 0.1f;
+    [SerializeField] float xScaleCorrection = 0.005f;
+    [SerializeField] float yScaleCorrection = 0.005f;
+
+    Vector3 standardLocalScale;
+    Vector3 maxLocalScale;
+    Vector3 minLocalScale;
+
     void Start()
     {
-        
+        standardLocalScale = transform.localScale;
+        maxLocalScale = standardLocalScale + new Vector3(xScaleExcess, yScaleExcess, 0);
+        minLocalScale = standardLocalScale - new Vector3(xScaleExcess, yScaleExcess, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // should probably make it so the things cant look bad
-        transform.localScale = transform.localScale + new Vector3(Random.Range(-.002f,.002f), Random.Range(-.002f,.002f), 0);
+        // should probably make it so the things can't look bad
+        
+        if (transform.localScale.x > maxLocalScale.x)
+        {
+            transform.localScale = transform.localScale - new Vector3(xScaleCorrection, 0, 0);
+        }
+        if (transform.localScale.x < minLocalScale.x)
+        {
+            transform.localScale = transform.localScale + new Vector3(xScaleCorrection, 0, 0);
+        }
+        if (transform.localScale.y > maxLocalScale.y)
+        {
+            transform.localScale = transform.localScale - new Vector3(0, yScaleCorrection, 0);
+        }
+        if (transform.localScale.y < minLocalScale.y)
+        {
+            transform.localScale = transform.localScale + new Vector3(0, yScaleCorrection, 0);
+        }
+        else
+        {
+            transform.localScale = transform.localScale + new Vector3(Random.Range(-xWobble, xWobble), Random.Range(-yWobble, yWobble), 0);
+        }
     }
 }
