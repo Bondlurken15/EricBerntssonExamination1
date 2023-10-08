6347861d36b2f74c271d8607bc38f96d0a050682
bonasieric <eric.berntsson@elev.ga.lbs.se>
2023-10-08 15:00:44 +0200

    Away with seek and variety3, in with the enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-public class BacteriaMovement : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    [Header("Position Wobble")]
-    [SerializeField] float xMove = 0.02f;
-    [SerializeField] float yMove = 0.02f;
+    [Header("Seek")]
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float speed = 0.005f;
     [Header("Scale Wobble")]
     [SerializeField] float xWobble = 0.002f;
     [SerializeField] float yWobble = 0.002f;
@@ -14,6 +15,9 @@ public class BacteriaMovement : MonoBehaviour
     [SerializeField] float yScaleExcess = 0.1f;
     [SerializeField] float xScaleCorrection = 0.005f;
     [SerializeField] float yScaleCorrection = 0.005f;
+    [Header("Position Wobble")]
+    [SerializeField] float xMove = 0.02f;
+    [SerializeField] float yMove = 0.02f;
 
     Vector3 standardLocalScale;
     Vector3 maxLocalScale;
@@ -28,9 +32,18 @@ public class BacteriaMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        Seek();
         Wobble();
     }
 
+    void Seek()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        }
+    }
+    
     void Wobble()
     {
         transform.position = transform.position + new Vector3(Random.Range(-xMove, xMove), Random.Range(-yMove, yMove));
@@ -54,6 +67,17 @@ public class BacteriaMovement : MonoBehaviour
         else
         {
             transform.localScale = transform.localScale + new Vector3(Random.Range(-xWobble, xWobble), Random.Range(-yWobble, yWobble));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Friendly")
+        {
+            print("got me");
+            Destroy(other.gameObject);
+            maxLocalScale += new Vector3(0.5f, 0.5f, 0);
+            transform.localScale += new Vector3(.5f, .5f, 0);
         }
     }
 }
